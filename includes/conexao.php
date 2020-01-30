ad6de2c63ac5f37c2ecaee37cdec666d12dbd640
Engels Souza <engels.franca@gmail.com>
2020-01-30 01:24:12 -0300

    banco está na nuvem, alterado forma de conexao no includes/conexao.php

--- includes/conexao.php
+++ includes/conexao.php
@@ -1,10 +1,11 @@
 <?php
-$local = 'localhost';
+$local = '191.232.52.106';
 $user = 'engels';
 $senha = 'engelsink666';
 $database = 'infopublic';
+$port = '45000';
 
-$conn = mysqli_connect($local, $user, $senha, $database);
+$conn = mysqli_connect($local, $user, $senha, $database, $port);
 if(!$conn){
 	die('Erro ao conectar-se com o banco de dados: ' . mysqli_connect_error());
 }
